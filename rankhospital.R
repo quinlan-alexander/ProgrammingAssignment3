7c6adeb3cafffed838660bde22d45cecf45640f1
quinlan-alexander <quinlan.alexander@gmail.com>
2016-09-01 18:17:05 -0700

    [qa] assignment complete

---
+++ rankhospital.R
@@ -0,0 +1,62 @@
+
+validState <- function(state, validStates) {
+  if (state  %in% validStates) {
+    return(TRUE)
+  }
+  else {
+    return(FALSE)
+  }
+}
+
+validOutcome <- function(outcome, validOutcomes) {
+  if (outcome %in% validOutcomes) {
+    return(TRUE)
+  }
+  else {
+    return(FALSE)
+  }
+}
+
+rankhospital <- function(state, outcome, num = "best") {
+  ## Read outcome data
+  ## setwd("/Users/ba25714/cousera/RProg/Week4")
+  outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
+  ## Coerce data
+  suppressWarnings(outcome_data[, 11] <- as.numeric(outcome_data[, 11]))
+  suppressWarnings(outcome_data[, 17] <- as.numeric(outcome_data[, 17]))
+  suppressWarnings(outcome_data[, 23] <- as.numeric(outcome_data[, 23]))
+  
+  ## Check that state and outcome are valid
+  validStates <- unique(outcome_data$State)
+  if (!validState(state, validStates)) {
+    stop("invalid state")
+  }
+  validOutcomes <- c("heart attack", "heart failure", "pneumonia")
+  if (!validOutcome(outcome, validOutcomes)) {
+    stop("invalid outcome")
+  }
+  
+  ## Create a data frame to map outcome to column number
+  outcomeColumnDf <- data.frame(Outcome = validOutcomes, 
+                                Col = c(11, 17, 23))
+  
+  ## Return hospital name in that state with lowest 30-day death rate
+  ##
+  stateDf <- outcome_data[outcome_data$State == state, ]
+  colNum <- outcomeColumnDf[outcomeColumnDf$Outcome == outcome, 2]
+  stateDf <- stateDf[order(stateDf[, colNum], stateDf$Hospital.Name), ]
+  stateDf <- stateDf[complete.cases(stateDf[,colNum]),]
+  
+  if (num == "best") {
+    num <- 1
+  } else if (num == "worst") {
+    num <- nrow(stateDf)
+  } else {
+    if (num > nrow(stateDf)) {
+      return("NA")
+    }
+  }
+  
+  stateDf[num,]$Hospital.Name
+}
